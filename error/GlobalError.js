1efff7947637d0edaee7fc8baf8ef4d0f9e85d28
emin <mustafazadeemin2000@gmail.com>
2023-02-04 01:27:05 +0400

    deleting all

--- error/GlobalError.js
+++
@@ -1,9 +0,0 @@
-class GlobalError extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-        this.operational = true;
-    };
-};
-
-module.exports = GlobalError;
--- utils/asyncCatch.js
+++
@@ -1,9 +0,0 @@
-const asyncCatch = (callback) => {
-    return (req, res, next) => {
-        callback(req, res, next).catch((err) => next(err));
-    };
-};
-
-module.exports = {
-    asyncCatch
-};
